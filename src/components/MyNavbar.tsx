f103e16a5b4fbf088a701befd7dfbb78d7d3fcf4
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2023-11-09 15:28:15 +0100

    navbar change

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -1,15 +1,14 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 const MyNavbar = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
+      <Container className="d-flex align-items-content ">
         <Navbar.Brand href="#home">
           <Link to="/" className="text-decoration-none text-black">
-            React-Bootstrap
+            Space Articles
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -20,17 +19,6 @@ const MyNavbar = () => {
                 Home
               </Link>
             </Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
